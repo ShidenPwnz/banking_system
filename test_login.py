6410ef78c0647eba05a6a02e9012fad4d6ec0d7f
ShidenPwnz <andrew.osepchuk@gmail.com>
2023-03-04 17:47:20 +0400

    commit import and create

---
+++ test_login.py
@@ -0,0 +1,40 @@
+import unittest
+from banking_system import login
+
+class TestLogin(unittest.TestCase):
+
+    def test_login(self):
+
+
+        # Test case 1: check if the function logs in a user with correct credentials
+        user_accounts = {"user1": "password1", "user2": "password2", "user3": "password3"}
+        log_in = {"user1": False, "user2": False, "user3": False}
+        username = "user1"
+        password = "password1"
+        expected_log_in = {"user1": True, "user2": False, "user3": False}
+        actual_log_in = log_in.copy()
+        login(user_accounts, actual_log_in, username, password)
+        self.assertEqual(actual_log_in, expected_log_in)
+
+        # Test case 2: check if the function rejects invalid credentials
+        user_accounts = {"user1": "password1", "user2": "password2", "user3": "password3"}
+        log_in = {"user1": False, "user2": False, "user3": False}
+        username = "user1"
+        password = "wrong_password"
+        expected_log_in = {"user1": False, "user2": False, "user3": False}
+        actual_log_in = log_in.copy()
+        login(user_accounts, actual_log_in, username, password)
+        self.assertEqual(actual_log_in, expected_log_in)
+
+        # Test case 3: check if the function handles non-existent usernames gracefully
+        user_accounts = {"user1": "password1", "user2": "password2", "user3": "password3"}
+        log_in = {"user1": False, "user2": False, "user3": False}
+        username = "non_existent_user"
+        password = "password1"
+        expected_log_in = {"user1": False, "user2": False, "user3": False}
+        actual_log_in = log_in.copy()
+        login(user_accounts, actual_log_in, username, password)
+        self.assertEqual(actual_log_in, expected_log_in)
+
+if __name__ == '__main__':
+    unittest.main()
